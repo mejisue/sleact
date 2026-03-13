9d9c2b465d65b62a1e260eaa7effb6bac2047a00
jisoo <februstar11@gmail.com>
2026-03-13 11:28:22 +0900

    first commit

---
+++ backend/src/main/java/mejisue/sleact/SleactApplication.java
@@ -0,0 +1,13 @@
+package mejisue.sleact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mejisue/sleact/SleactApplicationTests.java
@@ -0,0 +1,13 @@
+package mejisue.sleact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
