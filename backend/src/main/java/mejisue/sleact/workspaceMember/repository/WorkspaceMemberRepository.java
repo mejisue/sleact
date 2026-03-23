6c2a2409b2e2a54667c9994b5481f276327fc2de
jisoo <februstar11@gmail.com>
2026-03-23 18:26:17 +0900

    fix : WorkspaceMemberRepository findByUserId @Param 누락 수정

--- backend/src/main/java/mejisue/sleact/workspaceMember/repository/WorkspaceMemberRepository.java
+++ backend/src/main/java/mejisue/sleact/workspaceMember/repository/WorkspaceMemberRepository.java
@@ -4,12 +4,13 @@ package mejisue.sleact.workspaceMember.repository;
 import mejisue.sleact.workspaceMember.domain.WorkspaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface WorkspaceMemberRepository extends JpaRepository<WorkspaceMember, Long> {
 
     @Query("select wm from WorkspaceMember wm join fetch wm.workspace ws join fetch wm.user u where u.id = :userId")
-    List<WorkspaceMember> findByUserId(Long userId);
+    List<WorkspaceMember> findByUserId(@Param("userId") Long userId);
 }
 
