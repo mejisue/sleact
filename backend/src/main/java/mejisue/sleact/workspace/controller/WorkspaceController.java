48978f234efb55a3f8f0a95d3141144367c38cd5
jisoo <februstar11@gmail.com>
2026-03-23 18:01:57 +0900

    feat : 내 워크스페이스 목록 조회 API 구현

--- backend/src/main/java/mejisue/sleact/workspace/controller/WorkspaceController.java
+++ backend/src/main/java/mejisue/sleact/workspace/controller/WorkspaceController.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import mejisue.sleact.workspace.dto.WorkspaceCreateDto;
 import mejisue.sleact.workspace.dto.WorkspaceResDto;
 import mejisue.sleact.workspace.service.WorkspaceService;
+import mejisue.sleact.workspaceMember.domain.WorkspaceMember;
+import mejisue.sleact.workspaceMember.service.WorkspaceMemberService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/workspaces")
@@ -16,10 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkspaceController {
 
     private final WorkspaceService workspacesService;
+    private final WorkspaceMemberService workspaceMemberService;
 
     /** 워크스페이스 생성하기 **/
     @PostMapping
     public WorkspaceResDto createWorkspace(Authentication authentication, @RequestBody WorkspaceCreateDto createDto) {
         return workspacesService.createWorkspace(authentication, createDto);
+    }
+
+    /** 내 워크스페이스 목록 가져오기 **/
+    @GetMapping
+    public ResponseEntity<List<WorkspaceResDto>> getMyWorkspaces(Authentication authentication) {
+        String email = authentication.getName();
+        return ResponseEntity.ok(workspaceMemberService.findWorkspacesByEmail(email));
     }
 }
--- backend/src/main/java/mejisue/sleact/workspaceMember/repository/WorkspaceMemberRepository.java
+++ backend/src/main/java/mejisue/sleact/workspaceMember/repository/WorkspaceMemberRepository.java
@@ -3,8 +3,13 @@ package mejisue.sleact.workspaceMember.repository;
 
 import mejisue.sleact.workspaceMember.domain.WorkspaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WorkspaceMemberRepository extends JpaRepository<WorkspaceMember, Long> {
 
+    @Query("select wm from WorkspaceMember wm join fetch wm.workspace ws join fetch wm.user u where u.id = :userId")
+    List<WorkspaceMember> findByUserId(Long userId);
 }
 
